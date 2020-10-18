86def39114e36246392151f29cdcfd46676fa8d2
Egill Antonsson <EgillAntonsson@users.noreply.github.com>
2020-10-18 12:56:34 +0200

    Dev/update to ng 9 (#3)
    *Updating to ng 9 using guide https://update.angular.io/?v=8.2-9.1
    ** 'ng update @angular/core@9 @angular/cli@9 --force'. Using '--force' due to 'Package '@angular/http' has an incompatible peer dependency to '@angular/core' (requires '7.2.16' (extended), would install '9.1.12').
    ** Ran 'ng update @angular/material@9'.
    ** package.json: added 'postinstall': 'ngcc' to scripts blob
    * Built website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core'
 import { AppRoutingModule } from './app-routing.module'
 import { BrowserModule } from '@angular/platform-browser'
 import { HttpClientModule } from '@angular/common/http'
-import { MatRippleModule, MatButtonToggleModule } from '@angular/material'
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatRippleModule } from '@angular/material/core';
 import { Ng5SliderModule } from 'ng5-slider'
 
 import { AppComponent } from './app.component'
