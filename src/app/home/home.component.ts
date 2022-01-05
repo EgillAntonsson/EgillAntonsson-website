2ebf5f266fadd31df60fbe6d536d3f4f63cd8b74
Egill Antonsson <egillantonsson@gmail.com>
2022-01-05 10:25:40 +0100

    hid static html form and followed git repo.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { HttpHeaders, HttpClient } from '@angular/common/http'
+import { HttpHeaders, HttpClient, HttpParams } from '@angular/common/http'
 import { Component } from '@angular/core'
 import { FormGroup, Validators, FormBuilder } from '@angular/forms'
 
@@ -47,8 +47,12 @@ export class HomeComponent {
 		console.log('Send clicked')
 		console.log(this.commentForm)
 
-		const body = new URLSearchParams()
-		body.set('inputComment', 'this is a static input comment text string')
+		// const body = new URLSearchParams()
+		// body.set('inputComment', 'this is a static input comment text string')
+
+		const body = new HttpParams()
+		.set('form-name', 'blogCommentForm')
+		.append('inputComment', 'this is a fixed comment message, whoop')
 
 		const httpOptions  = {
 			headers: new HttpHeaders({
@@ -56,7 +60,33 @@ export class HomeComponent {
 			})
 		}
 
-		this.http.post('/', body, httpOptions)
+		const url = '/'
+		console.log(url)
+
+		this.http.post(url, body.toString, httpOptions).subscribe(
+			res => {
+				console.log('post res')
+				console.log(res)
+			},
+			err => {
+				if (err instanceof ErrorEvent) {
+					// client side error
+					alert('Something went wrong when sending your message.')
+					console.log(err.error.message)
+				} else {
+					// backend error. If status is 200, then the message successfully sent
+					if (err.status === 200) {
+						alert('Your message has been sent!, but backend error')
+					} else {
+						alert('Something went wrong when sending your message (backend error).');
+						console.log('Error status:')
+						console.log(err.status)
+						console.log('Error body:')
+						console.log(err.error)
+					}
+				}
+			}
+		)
 
 		console.log('after posting')
 
