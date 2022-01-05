0e867095344868cc186fc56f905e86428a77bb92
Egill Antonsson <egillantonsson@gmail.com>
2022-01-05 17:10:23 +0100

    WIP: strong typing format variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,49 +9,46 @@ import { HttpParams, HttpClient } from '@angular/common/http'
 export class AppComponent {
 
 	commentForm: FormGroup
-	inputCommentControl
-	inputNameControl
-	inputEmailControl
+	// inputCommentControl
+	// inputNameControl
 
 	defaultCommentText = `Write your comment here.
-	On 'Send' button press it will be pending for moderation.
-	On approval I'll publish the comment here.
-	(I might also add a short response).
-	`
+On 'Send' button press it will be pending for moderation.
+On approval I'll publish the comment here.
+(I might also add a short response).`
 	defaultNameText = 'Anonymous'
 	defaultEmailText = 'email@domain.com'
 
 	showCommentForm = true
 
+	formName = 'blogCommentForm'
+	commentKey = 'comment'
+
 	constructor(private fb: FormBuilder, private http: HttpClient) {
 		this.commentForm = this.fb.group({
-			inputComment: ['', [
-					Validators.required
-				]
-			],
-			inputName: ['', []],
-			inputEmail: ['', [
-				Validators.email
-			]
-		],
+			[this.commentKey]: ['', [Validators.required]],
+			inputName: ['', [Validators.required]
+		]
 		})
-		this.inputCommentControl = this.commentForm.controls['inputComment']
-		this.inputNameControl = this.commentForm.controls['inputName']
-		this.inputEmailControl = this.commentForm.controls['inputEmail']
+		// this.inputCommentControl = this.commentForm.controls['inputComment']
+		// this.inputNameControl = this.commentForm.controls['inputName']
 	}
 
 	onSendClick() {
-		console.log('Send clicked')
-		console.log(this.commentForm)
 
-		console.log(this.inputCommentControl)
+		// console.log(this.inputCommentControl)
 
 		const body = new HttpParams()
-		.set('form-name', 'blogCommentForm')
-		.append('inputComment', this.inputCommentControl.value)
+		.set('form-name', this.formName)
+		.append('inputComment', this.commentForm.value[this.commentKey])
 
 		const url = '/'
 		console.log(url)
+
+		console.log('Send clicked')
+		console.log(this.commentForm)
+		console.log('body')
+		console.log(body)
 
 		this.http.post(url, body.toString(), {headers: { 'Content-Type': 'application/x-www-form-urlencoded' }}).subscribe(
 			res => {
@@ -60,15 +57,15 @@ export class AppComponent {
 			err => {
 				if (err instanceof ErrorEvent) {
 					// client side error
-					alert('Something went wrong when sending your comment.')
+					console.log('Something went wrong when sending your comment')
 					console.log('client side error:')
 					console.log(err.error.message)
 				} else {
 					// backend error. If status is 200, then the message successfully sent
 					if (err.status === 200) {
-						alert('Your message has been sent')
+						console.log('Your comment has been sent')
 					} else {
-						alert('Something went wrong when sending your comment')
+						console.log('Something went wrong when sending your comment')
 						console.log('backend side error')
 						console.log('Error status:')
 						console.log(err.status)
