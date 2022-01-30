b27bae55108f73ef69673d3e297c7025ea2e675c
Egill Antonsson <egillantonsson@gmail.com>
2022-01-30 17:31:13 +0100

    Finished putting in Fabio's comment. Updated a bit tdd1 (adding Prototype Code section).

--- src/app/shared/services/blog.service.ts
+++ src/app/shared/services/blog.service.ts
@@ -14,21 +14,8 @@ export class BlogService {
 
 		const seriesTddTitle = 'TDD-ing Avatar Health in Unity (C# .NET)'
 		const dateYear = '2021'
-		const dateMonth = 'December'
+		const dateMonth = 'Dec'
 		const dateDay = '30'
-
-		const part1Comments = Array<Comment>()
-		part1Comments.push( {
-			handle: 'Fabio Paes Pedro',
-			comment:
-`I'm no expert but the value TDD provides became clear to me quite fast.
-I believe this requires a mindset/process change though, a different way of thinking things through that might extend outside of our team, into a company level.
-I can't wait to read the rest of this series!`,
-			myResponse:
-`Thank you for your comment Fabio.
-I agree that TDD requires a mindset/process change, where ideally the whole team and the company are aligned on using TDD to reap its benefits (maybe synced on a sensible approach, e.g. that the value of TDD dwindles near boundary edges).
-I aim to continue writing blog post parts and complete this series at an appropriate point, stay tuned :)`
-})
 
 		this.series = [
 			{title: seriesTddTitle, posts: [
@@ -43,11 +30,9 @@ I aim to continue writing blog post parts and complete this series at an appropr
 		this.selectedPost = this.series[0].posts[0]
 
 		this.posts = []
-		let index = 0
 		for (let i = 0; i < this.series.length; i++) {
 			for (let j = 0; j < this.series[i].posts.length; j++) {
 				const post = this.series[i].posts[j]
-				post.index = index++
 				this.posts.push(post)
 			}
 		}
@@ -72,7 +57,6 @@ export class Post {
 	readonly dateYear: string
 	readonly dateMonth: string
 	readonly dateDay: string
-	index!: number
 
 	constructor(title: string, routePath: string, dateYear: string, dateMonth: string, dateDay: string, seriesTitle: string) {
 		this.title = title
@@ -92,6 +76,6 @@ export interface Series {
 export interface Comment {
 	handle: string
 	comment: string
-	myResponse: string
+	response: string
 }
 
