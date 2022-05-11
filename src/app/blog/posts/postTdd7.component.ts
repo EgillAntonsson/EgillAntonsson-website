5c87b99388d468a4b8093ba4681999f87916de18
Egill Antonsson <egillantonsson@gmail.com>
2022-05-11 11:37:44 +0200

    IncreaseByUnit FullPointsIncrease: refactor (add test cases) step done. Have started drafting next steps.

--- src/app/blog/posts/postTdd7.component.ts
+++ src/app/blog/posts/postTdd7.component.ts
@@ -14,9 +14,9 @@ export class PostTdd7Component extends PostComponent {
 [Test]
 public void FullPointsIncrease()
 {
-	var health = new Health(12);
+	var health = new Health(4);
 	health.IncreaseByUnit(1);
-	Assert.That(health.FullPoints, Is.EqualTo(16));
+	Assert.That(health.FullPoints, Is.EqualTo(8));
 }
 `
 
@@ -36,14 +36,36 @@ public void IncreaseByUnit(int unit)
 }
 `
 
+refactor_fullPointsIncrease_addTests = `// Health.cs
+[TestCase(8, 4, 1)]
+[TestCase(12, 4, 2)]
+[TestCase(16, 4, 3)]
+public void FullPoints_WhenStartingPoints_ThenIncreaseByUnit(
+	int fullPoints,
+	int startingPoints,
+	int unit)
+{
+	var health = new Health(startingPoints);
+	health.IncreaseByUnit(unit);
+	Assert.That(health.FullPoints, Is.EqualTo(fullPoints));
+}
+`
+
 	red_fullPointsIncrease_currentPoints = `// HealthTest.cs
 // inside nested class IncreaseByUnit
 [Test]
-public void FullPointsIncrease()
+public void CurrentPointsIncrease()
 {
 	var health = new Health(12);
 	health.IncreaseByUnit(1);
-	Assert.That(health.FullPoints, Is.EqualTo(16));
+	Assert.That(health.CurrentPoints, Is.EqualTo(16));
+}
+`
+
+	green_fullPointsIncrease_currentPoints = `// Health.cs
+public void IncreaseByUnit(int unit)
+{
+	FullPoints += unit * 4;
 }
 `
 
