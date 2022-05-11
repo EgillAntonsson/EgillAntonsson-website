ff5402da411f4d3d3d16ae19f2633327c3446fef
Egill Antonsson <egillantonsson@gmail.com>
2022-05-11 12:20:59 +0200

    code for CurrentPoints exit point red green cycles done.

--- src/app/blog/posts/postTdd7.component.ts
+++ src/app/blog/posts/postTdd7.component.ts
@@ -37,6 +37,7 @@ public void IncreaseByUnit(int unit)
 `
 
 refactor_fullPointsIncrease_addTests = `// Health.cs
+// inside nested class IncreaseByUnit
 [TestCase(8, 4, 1)]
 [TestCase(12, 4, 2)]
 [TestCase(16, 4, 3)]
@@ -51,21 +52,75 @@ public void FullPoints_WhenStartingPoints_ThenIncreaseByUnit(
 }
 `
 
+red_fullPointsIncrease_invalidInput = `// HealthTest.cs
+// inside nested class IncreaseByUnit
+[TestCase(0)]
+[TestCase(-1)]
+public void ThrowsError_WhenUnitParamIsInvalid(int unit)
+{
+	var health = new Health(4);
+	var exception = Assert.Throws(Is.TypeOf<ArgumentOutOfRangeException>(),
+		delegate
+		{
+			health.IncreaseByUnit(unit);
+		});
+	Assert.That(exception.Message, Does.Match("invalid").IgnoreCase);
+}
+`
+
+green_fullPointsIncrease_invalidInput = `// HealthTest.cs
+public void IncreaseByUnit(int unit)
+{
+	ValidatePoints(unit, 1); // method not shown
+	FullPoints += unit * PointsPerUnit;
+}
+`
+
 	red_fullPointsIncrease_currentPoints = `// HealthTest.cs
 // inside nested class IncreaseByUnit
 [Test]
 public void CurrentPointsIncrease()
 {
-	var health = new Health(12);
+	var health = new Health(4);
 	health.IncreaseByUnit(1);
-	Assert.That(health.CurrentPoints, Is.EqualTo(16));
+	Assert.That(health.CurrentPoints, Is.EqualTo(8));
 }
 `
 
 	green_fullPointsIncrease_currentPoints = `// Health.cs
 public void IncreaseByUnit(int unit)
 {
-	FullPoints += unit * 4;
+	ValidatePoints(unit, 1);
+	FullPoints += unit * PointsPerUnit;
+	CurrentPoints = FullPoints;
+}
+`
+
+red_fullPointsIncrease_currentPoints_moreCases = `// HealthTest.cs
+// inside nested class IncreaseByUnit
+[TestCase(7, 4, 1, 1)]
+[TestCase(6, 4, 2, 1)]
+[TestCase(11, 4, 1, 2)]
+[TestCase(10, 4, 2, 2)]
+public void CurrentPoints_WhenStartingPoints_ThenDamagePoints_ThenIncreaseByUnit(
+	int currentPoints,
+	int startingPoints,
+	int damagePoints,
+	int unit)
+{
+	var health = new Health(startingPoints);
+	health.TakeDamage(damagePoints);
+	health.IncreaseByUnit(unit);
+	Assert.That(health.CurrentPoints, Is.EqualTo(currentPoints));
+}
+`
+
+green_fullPointsIncrease_currentPoints_moreCases = `// Health.cs
+public void IncreaseByUnit(int unit)
+{
+	ValidatePoints(unit, 1);
+	FullPoints += unit * PointsPerUnit;
+	CurrentPoints += unit * PointsPerUnit;
 }
 `
 
