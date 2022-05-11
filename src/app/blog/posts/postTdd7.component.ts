e0f691758659152d6949c248851e66fa2a960f88
Egill Antonsson <egillantonsson@gmail.com>
2022-05-11 16:44:59 +0200

    simplified requirement to increase to 1 Unit AT A TIME, and refactored code and text accordingly. WIP start of maximum.

--- src/app/blog/posts/postTdd7.component.ts
+++ src/app/blog/posts/postTdd7.component.ts
@@ -14,16 +14,16 @@ export class PostTdd7Component extends PostComponent {
 [Test]
 public void FullPointsIncrease()
 {
-	var health = new Health(4);
-	health.IncreaseByUnit(1);
-	Assert.That(health.FullPoints, Is.EqualTo(8));
+	var health = new Health(12);
+	health.IncreaseByUnit();
+	Assert.That(health.FullPoints, Is.EqualTo(16));
 }
 `
 
 	green_fullPointsIncrease = `// Health.cs
-public void IncreaseByUnit(int unit)
+public void IncreaseByUnit()
 {
-	FullPoints += unit * 4;
+	FullPoints += 4;
 }
 `
 
@@ -32,47 +32,7 @@ public const int PointsPerUnit = 4;
 
 public void IncreaseByUnit(int unit)
 {
-	FullPoints += unit * PointsPerUnit;
-}
-`
-
-	refactor_fullPointsIncrease_addTests = `// Health.cs
-// inside nested class IncreaseByUnit
-[TestCase(8, 4, 1)]
-[TestCase(12, 4, 2)]
-[TestCase(16, 4, 3)]
-public void FullPoints_WhenStartingPoints_ThenIncreaseByUnit(
-	int fullPoints,
-	int startingPoints,
-	int unit)
-{
-	var health = new Health(startingPoints);
-	health.IncreaseByUnit(unit);
-	Assert.That(health.FullPoints, Is.EqualTo(fullPoints));
-}
-`
-
-	red_fullPointsIncrease_invalidInput = `// HealthTest.cs
-// inside nested class IncreaseByUnit
-[TestCase(0)]
-[TestCase(-1)]
-public void ThrowsError_WhenUnitParamIsInvalid(int unit)
-{
-	var health = new Health(4);
-	var exception = Assert.Throws(Is.TypeOf<ArgumentOutOfRangeException>(),
-		delegate
-		{
-			health.IncreaseByUnit(unit);
-		});
-	Assert.That(exception.Message, Does.Match("invalid").IgnoreCase);
-}
-`
-
-	green_fullPointsIncrease_invalidInput = `// HealthTest.cs
-public void IncreaseByUnit(int unit)
-{
-	ValidatePoints(unit, 1); // method not shown
-	FullPoints += unit * PointsPerUnit;
+	FullPoints += PointsPerUnit;
 }
 `
 
@@ -81,56 +41,42 @@ public void IncreaseByUnit(int unit)
 [Test]
 public void CurrentPointsIncrease()
 {
-	var health = new Health(4);
-	health.IncreaseByUnit(1);
-	Assert.That(health.CurrentPoints, Is.EqualTo(8));
+	var health = new Health(12);
+	health.IncreaseByUnit();
+	Assert.That(health.CurrentPoints, Is.EqualTo(16));
 }
 `
 
 	green_fullPointsIncrease_currentPoints = `// Health.cs
 public void IncreaseByUnit(int unit)
 {
-	ValidatePoints(unit, 1);
-	FullPoints += unit * PointsPerUnit;
+	FullPoints += PointsPerUnit;
 	CurrentPoints = FullPoints;
 }
 `
 
 	red_fullPointsIncrease_currentPoints_moreCases = `// HealthTest.cs
 // inside nested class IncreaseByUnit
-[TestCase(7, 4, 1, 1)]
-[TestCase(6, 4, 2, 1)]
-[TestCase(11, 4, 1, 2)]
-[TestCase(10, 4, 2, 2)]
-public void CurrentPoints_WhenStartingPoints_ThenDamagePoints_ThenIncreaseByUnit(
+[TestCase(7, 4, 1)]
+[TestCase(6, 4, 2)]
+[TestCase(5, 4, 3)]
+public void CurrentPoints_WhenStartingPoints_ThenDamagePoints(
 	int currentPoints,
 	int startingPoints,
-	int damagePoints,
-	int unit)
+	int damagePoints)
 {
 	var health = new Health(startingPoints);
 	health.TakeDamage(damagePoints);
-	health.IncreaseByUnit(unit);
+	health.IncreaseByUnit();
 	Assert.That(health.CurrentPoints, Is.EqualTo(currentPoints));
 }
 `
 
 	green_fullPointsIncrease_currentPoints_moreCases = `// Health.cs
-public void IncreaseByUnit(int unit)
+public void IncreaseByUnit()
 {
-	ValidatePoints(unit, 1);
-	FullPoints += unit * PointsPerUnit;
-	CurrentPoints += unit * PointsPerUnit;
-}
-`
-
-	refactor_fullPointsIncrease_currentPoints_moreCases = `// Health.cs
-public void IncreaseByUnit(int unit)
-{
-	ValidatePoints(unit, 1);
-	var pointsIncrease = unit * PointsPerUnit;
-	FullPoints += pointsIncrease;
-	CurrentPoints += pointsIncrease;
+	FullPoints += PointsPerUnit;
+	CurrentPoints += PointsPerUnit;
 }
 `
 
