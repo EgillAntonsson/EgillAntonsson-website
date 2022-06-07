889bd1b5c3f5b5e93cef879d62acb192e3fa86f8
Egill Antonsson <egillantonsson@gmail.com>
2022-06-07 09:26:00 +0200

    Add about text, artwork, and links for the first 3 tracks. Used Location to set url properly when track is played. Updated sitemap accordingly.

--- src/app/shared/data/track.ts
+++ src/app/shared/data/track.ts
@@ -41,7 +41,7 @@ export class Track implements ITrack {
 	constructor(name: string
 		, soundDatas: SoundData[]
 		, play: () => () => Promise<void>
-		, rootUrl: string = 'not-defined'
+		, rootUrl: string = ''
 		, artworkPath: string = ''
 		, about: string = ''
 		, soundcloudUrl: string = ''
