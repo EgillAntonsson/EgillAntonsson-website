1c3b35ae046c5e3ae191d1a1edb90b2dcd219c9d
Egill Antonsson <6976127+EgillAntonsson@users.noreply.github.com>
2022-11-28 08:31:59 +0100

    Update to ng 15 (#63)
    * after command 'ng update @angular/core@15 @angular/cli@15 --force'
    * ran 'ng update @angular/material@15'
    * Afterwards everything works except no unit tests are found. Will fix that when next time touching the logic.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -37,7 +37,7 @@ const routes: Routes = [
 ]
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes, {enableTracing: false, relativeLinkResolution: 'legacy' })],
+	imports: [RouterModule.forRoot(routes, { enableTracing: false })],
 	exports: [RouterModule]
 })
 
