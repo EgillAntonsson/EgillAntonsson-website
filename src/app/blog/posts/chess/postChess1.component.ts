b711140c0bf4554415d92fa667d22dc9ce128253
Egill Antonsson <6976127+EgillAntonsson@users.noreply.github.com>
2023-10-22 23:25:37 +0200

    Done with Position invalid values

--- src/app/blog/posts/chess/postChess1.component.ts
+++ src/app/blog/posts/chess/postChess1.component.ts
@@ -9,50 +9,20 @@ import { PostComponent } from '../post.component'
 
 export class PostChess1Component extends PostComponent {
 
-	hasPositionRedDoesNotCompileText() {
-		return `<p>I create the test file and write the first test and see it fail as the code does not compile.
-</p>`
-	}
-
-	hasPositionRedDoesNotCompileCode() {
-		return `// KingTest.cs
-using NUnit.Framework;
-
-public class KingTest
-{
-	public class Constructor
-	{
-		[Test]
-		public void Position_IsSet()
-		{
-			var row = 2;
-			var column = 3;
-			var king = new King(row, column);
-			Assert.That(king.Row, Is.EqualTo(row));
-			Assert.That(king.Column, Is.EqualTo(column));
-		}
-	}
+PositionRowColRedCompileText() {
+	return `<p>I create the test file and write the first test and see it fail, as the code does not compile.</p>`
 }
-`
-	}
-	PositionRowColGreenText() {
-		return `<p>It is effortless to make the test pass, and jump over step <i>Red (test fails)</i>, as my IDE (<a href="https://www.jetbrains.com/rider/" target="_blank">Jetbrains Rider</a>) and AI tool (<a href="https://github.com/features/copilot/" target="_blank">Github Copilot</a>) assist me as I start to type.</p>`
-	}
 
-	PositionRowColRedText() {
-		return `<p><p>I create the test file and write the first (very basic) test and see it fail as the code does not compile.</p>`
-	}
-
-	PositionRowColRedCode() {
+PositionRowColRedCompileCode() {
 		return `// PositionTest.cs
 using NUnit.Framework;
 
-public class PositionTest
+public abstract class PositionTest
 {
 	public class Constructor
 	{
 		[Test]
-		public void RowAndColumn_Initialized()
+		public void RowAndColumn_AreInitialized()
 		{
 			var position = new Position(0, 1);
 			Assert.That(position.Row, Is.EqualTo(0));
@@ -61,7 +31,35 @@ public class PositionTest
 	}
 }
 `
+}
+
+PositionRowColRedTestFailsText() {
+	return `<p>I write the minimal production code to compile successfully and intentionally make the test fail to verify that the test is not passing when it should not.</p>`
+}
+
+PositionRowColRedTestFailsCode() {
+	return `// Position.cs
+public struct Position
+{
+	public int Row;
+	public int Column;
+
+	public Position(int row, int column)
+	{
+		Row = -1;
+		Column = -1;
 	}
+}
+`
+}
+
+PositionRowColGreenText() {
+	return `<p>It make the test pass by providing the obvious solution that holds for all cases.</p>`
+}
+
+PositionRowColGreenText1() {
+	return `<p>The only thing I deem to refactor is encapsulating the properties with getters.</p>`
+}
 
 	PositionRowColGreenCode() {
 		return `// Position.cs
@@ -77,11 +75,11 @@ public struct Position
 	}
 }
 `
-	}
+}
 
-	PositionRowColGreenText1() {
-		return `<p>I think it fits well to have <i>Position</i> as <a href="https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/structs#161-general" target="_blank">Struct</a>, and I encapsulate the properties with getters (if later it turns out that the domain design needs setters then they'll be TDD cycled in).</p>`
-	}
+PositionRowInvalidRedText() {
+	return `<p>I add the test for invalid row parameter value and see it fail.</p>`
+}
 
 PositionRowInvalidRedCode() {
 	return `// PositionTest.cs
@@ -98,10 +96,12 @@ public void Error_WhenRowParamIsInvalid()
 `
 }
 
+PositionRowInvalidGreenText() {
+	return `<p>It make the test pass by providing the obvious solution that holds for all cases, and provide the bare minimum exception message to satisfy the test.</p>`
+}
+
 PositionRowInvalidGreenCode() {
 	return `// Position.cs
-using System;
-
 public Position(int row, int column)
 {
 	if (row < 0)
@@ -112,6 +112,10 @@ public Position(int row, int column)
 	Column = column;
 }
 `
+}
+
+PositionRowInvalidRefactorText() {
+	return `<p>I define <i>lowestValidValue</i> and use it both in the condition and the message. This makes the message informative and makes sure the conditional and message will always be in sync. I verify the message displays as expected (with debug breakpoint): Value -1 is invalid, it should be equal or higher than 0. Parameter name: row.</p>`
 }
 
 PositionRowInvalidRefactorCode() {
@@ -129,6 +133,10 @@ public Position(int row, int column)
 `
 }
 
+PositionColInvalidRedText() {
+	return `<p>I add the test for invalid column parameter value and see it fail.</p>`
+}
+
 PositionColInvalidRedCode() {
 	return `// PositionTest.cs
 [Test]
@@ -143,6 +151,13 @@ public void Error_WhenColumnParamIsInvalid()
 }
 `
 }
+
+PositionColInvalidGreenText() {
+	return `<p>As the Green step is all about a quick implementation that makes the test(s) pass, I temporarily commit the code sin of duplication, as I will refactor it clean in the next step.</p>`
+}
+// PositionColInvalidGreenText() {
+// 	return `<p>This naive implementation should make the developer's 'spider sense' kick in as it clearly will not hold for most cases, but this step is about testing the test, and implementation will improved in the next step.</p>`
+// }
 
 PositionColInvalidGreenCode() {
 	return `// Position.cs
@@ -196,8 +211,37 @@ public readonly struct Position
 `
 }
 
-	positionEqualityRefactorText() {
-		return `<p>The equality test assertions on the position work because any struct has a default implementation of value equality. I'll paraphrase from the <a href="https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/statements-expressions-operators/how-to-define-value-equality-for-a-type" target="_blank">Microsoft doc</a>: "Any struct has a default implementation of value equality that it inherits from the <i>System.ValueType</i>. This implementation uses reflection which is relatively slow compared to a custom implementation that you write specifically for the type."</p>`
+PositionColInvalidRefactorText() {
+	return `<p>I refactor out the duplication and improve the code, by taking small steps and running all the tests after each step.</p>`
+}
+
+PositionColInvalidRefactorText1() {
+	return `<p>The refactor steps were in broad strokes the below:</p><ul class="list inline">
+<li>I highlight the row condition case and <a href="https://refactoring.guru/extract-method" target="_blank">Extract Method</a> (with my IDE) and name it <i>ValidateCoord</i> and change the <i>row</i> to <i>coord</i></li>
+<li>I create method <i>Validate</i> and make the constructor call it after setting the properties, this method calls <i>ValidateCoord</i> with param <i>Row</i> and then again with param <i>Column</i></li>
+<li>My IDE suggests to make the struct as <i>readonly</i> and that sounds proper so I apply it</li>
+<li>When the exception is thrown you get the stack trace in the console and therefore it is sufficient that the param name will simply be <i>coord</i> as you can follow the trace to see which line called <i>ValidateCoord</i></li>
+<li>For <i>PositionTest</i> I update that the <i>ParamName</i> should match "coord" (instead of "row" or "column")</li></ul>`
+}
+
+
+positionsAreEqualIntroText() {
+	return `<p>I foresee that the Domain logic will be checking <i>Positions</i> for equality. So I write a test for it.</p>`
+}
+
+PositionsAreEqualTestCode() {
+	return `
+[Test]
+public void AreEqual_WhenWithSameRowAndSameColumn()
+{
+	var position = new Position(2, 3);
+	Assert.That(position, Is.EqualTo(new Position(2, 3)));
+}
+`
+}
+
+PositionsAreEqualGreenText() {
+		return `<p>The test passes without any implementation from me because any struct has a default implementation of value equality. Paraphrasing from the <a href="https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/statements-expressions-operators/how-to-define-value-equality-for-a-type" target="_blank">Microsoft doc</a>: "Any struct has a default implementation of value equality that it inherits from the <i>System.ValueType</i>. This implementation uses reflection which is relatively slow compared to a custom implementation that you write specifically for the type."</p>`
 	}
 
 	positionEqualityRefactorText1() {
@@ -220,45 +264,4 @@ Assert.That(position, Is.EqualTo(new Position(2, 3)));
 `
 	}
 
-	GetMoveDefinitionsRedText() {
-		return `<p>The King is capable of moving one square in whatever direction (if nothing prevents the move). Thus the King class will have <i>GetMoveDefinition</i> method that returns a collection of <i>Positions</i> that are relative</p>`
-	}
-
-	GetMoveDefinitionsRedCode() {
-		return `// KingTest.cs
-public class GetMoveDefinitions
-{
-	[Test]
-	public void Moves_AreReturned()
-	{
-		var position = new Position(2, 3);
-		var king = new King(position);
-		var moves = king.GetMoveDefinitions();
-		Assert.That(moves, Is.EquivalentTo(new[]
-		{
-			new Position(1, 0),
-			new Position(-1, 0),
-			new Position(0, 1),
-			new Position(0, -1),
-			new Position(1, 1),
-			new Position(-1, 1),
-			new Position(1, -1),
-			new Position(-1, -1)
-		}));
-	}
-}
-`
-	}
-
-	getMoveDefinitionsGreenText() {
-		return `Green (test passes)`
-	}
-
-	getMoveDefinitionsGreenCode() {
-		return `// King.cs
-public Position[] GetMoveDefinitions()
-{
-	return new[] { new Position(1, 0), };
-}`
-	}
 }
